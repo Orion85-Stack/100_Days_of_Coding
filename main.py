a4dd46a28ff450973a5770d8a5433862b681e52c
StephenMare <3753334-StephenMare@users.noreply.replit.com>
2023-05-15 17:38:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print('Day 1 - Python Print Function')
+print('The functiuon is declared like this:')
+print("print('what to print')")
+print("For Fun : Hello World!\nHello World\n" + "Angela")
+
+a = "Test"
+print(a)
